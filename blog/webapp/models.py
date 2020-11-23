1a0c4b66eea9db23e8146ab8c079adcc8fea7d81
PauloMorillo <pauloan@hotmail.com>
2020-11-23 12:37:17 -0500

    [upodate] added models and regiter Post in admin

---
+++ blog/webapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="user_author")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    publish = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    def __str__(self):
+        return self.title
